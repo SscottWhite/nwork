ac5d3e59d3d4b16176655678c4d39b3152277ccb
nicheng <179813964@qq.com>
2018-10-25 14:30:33 +0800

    udpate--研究插入排序

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.utils.ExcelUtil;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import java.sql.Array;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TestController {
 
     /*public static void main(String[] args) {
@@ -22,7 +29,7 @@ public class TestController {
         }
     }*/
 
-    //一个循环
+    /*//一个循环
     public static void main(String[] args) {
         int array[] = new int[]{2,3,41,4,21,1,20};
         //System.out.println(array.length);
@@ -44,5 +51,55 @@ public class TestController {
         for (int i : array) {
             System.out.println(i);
         }
+    }*/
+
+    public static void main(String[] args) {
+        //插入排序
+        int array[] = new int[]{49,38,65,97,76,13,27,49,78,34,12,64,1};
+        for(int i = 1; i<array.length ; i++){
+            int temp = array[i];
+            int j;
+            for(j = i-1;j>=0;j--){
+                if(array[j] > temp){
+                    array[j+1] = array[j];
+                }else{
+                    break;
+                }
+            }
+            array[j+1] = temp;
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
+
+   // public static void main(String[] args) {
+        /*List<List<String>> data = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        list.add("网点编号");
+        list.add("货号");
+        list.add("色号");
+        data.add(list);
+        list.clear();
+        list.add("ZB92");
+        list.add("X80130006B");
+        list.add("1132");
+        data.add(list);
+
+       // ExcelUtil.downloadXLS();
+     }*/
+        /*Map map = Collections.synchronizedMap(new LinkedHashMap<>());//Link不安全,可以用同步包
+        //Map map1 = Collections.singletonMap(new HashMap<>())
+        Map map2 = new ConcurrentHashMap();
+
+        Double x = Math.ceil(1040/500);
+        System.out.println(x);*/
+        /*int i = 8;
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        if(list.contains(i)){
+            System.out.println("good");
+        }else{
+            System.out.println("nogood");
+        }*/
+   // }
 }
